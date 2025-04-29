0d58bedbd3e41ce13df8fafcda9d1b0d56a361cc
Clever_Rojas <N00387770@upn.pe>
2025-04-28 23:15:58 -0500

    Commit Clever Rojas

--- ProyectoFinal/src/InventarioPeluches/ArregloPeluches.java
+++ ProyectoFinal/src/InventarioPeluches/ArregloPeluches.java
@@ -7,14 +7,14 @@ public class ArregloPeluches {
 	private ArrayList<Peluches>pelu;
 	public ArregloPeluches() {
 		pelu=new ArrayList<Peluches>();
-	Adicionar(new Peluches(100,"Eevee",20,55.0));
+	Adicionar(new Peluches(100,"Eevee",22,55.0));
 	Adicionar(new Peluches(110,"Espeon",20,45.0));
-	Adicionar(new Peluches(120,"Glaceon",20,80.0));
+	Adicionar(new Peluches(120,"Glaceon",22,80.0));
 	Adicionar(new Peluches(130,"Sylveon",20,25.0));
 	Adicionar(new Peluches(140,"Jolteon",20,10.0));
 	Adicionar(new Peluches(150,"Vaporeon",20,70.0));
 	Adicionar(new Peluches(160,"Leafeon",20,80.0));
-	Adicionar(new Peluches(170,"Flareon",20,48.0));
+	Adicionar(new Peluches(170,"Flareon",20,89.90));
 	Adicionar(new Peluches(180,"umbreon",20,60.0));
 	}
 	
